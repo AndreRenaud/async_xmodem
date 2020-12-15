5953cdb170b58ff52d7666f033163182046f0fa1
Andre Renaud <andre@ignavus.net>
2020-12-15 21:35:15 +1300

    Enable support for 1k transfers

--- xmodem_server_test.c
+++ xmodem_server_test.c
@@ -18,9 +18,16 @@ static void tx_byte(struct xmodem_server *xdm, uint8_t byte, void *cb_data)
 
 static bool rx_packet(struct xmodem_server *xdm, const uint8_t *data, int data_len, int block_nr, int error_rate) {
 	uint16_t crc = 0;
-	if (data_len > XMODEM_PACKET_SIZE)
-		data_len = XMODEM_PACKET_SIZE;
-	xmodem_server_rx_byte(xdm, 0x01);
+	uint8_t tx_type;
+	if (data_len == 128)
+		tx_type = 0x01;
+	else if (data_len == 1024)
+		tx_type = 0x02;
+	else {
+		fprintf(stderr, "Invalid data_len: %d\n", data_len);
+		return false;
+	}
+	xmodem_server_rx_byte(xdm, tx_type);
 	xmodem_server_rx_byte(xdm, block_nr + 1);
 	xmodem_server_rx_byte(xdm, (block_nr + 1) ^ 0xff);
 	for (int i = 0; i < data_len; i++) {
@@ -29,10 +36,6 @@ static bool rx_packet(struct xmodem_server *xdm, const uint8_t *data, int data_l
 			b ^= rand() & 0xff;
 		xmodem_server_rx_byte(xdm, b);
 		crc = xmodem_server_crc(crc, data[i]);
-	}
-	for (int i = data_len; i < XMODEM_PACKET_SIZE; i++) {
-		xmodem_server_rx_byte(xdm, 0xff);
-		crc = xmodem_server_crc(crc, 0xff);
 	}
 	xmodem_server_rx_byte(xdm, crc >> 8);
 	return xmodem_server_rx_byte(xdm, crc & 0xff);
@@ -49,23 +52,23 @@ static void test_simple(void) {
 	struct xmodem_server xdm;
 	uint8_t tx_char = 0;
 
-	TEST_CHECK(xmodem_server_init(&xdm, tx_byte, &tx_char) >= 0);
+	TEST_ASSERT(xmodem_server_init(&xdm, tx_byte, &tx_char) >= 0);
 	for (uint32_t i = 0; i < 5; i++) {
-		uint8_t data[XMODEM_PACKET_SIZE];
-		uint8_t resp[XMODEM_PACKET_SIZE];
+		uint8_t data[128];
+		uint8_t resp[128];
 		uint32_t block_nr;
 		memset(data, i, sizeof(data));
-		TEST_CHECK(rx_packet(&xdm, data, sizeof(data), i, 0));
+		TEST_ASSERT(rx_packet(&xdm, data, sizeof(data), i, 0));
 		// Should be fine to receive the same packet twice
-		TEST_CHECK(rx_packet(&xdm, data, sizeof(data), i, 0));
-		TEST_CHECK(xmodem_server_process(&xdm, resp, &block_nr, ms_time()));
-		TEST_CHECK(memcmp(data, resp, sizeof(data)) == 0);
-		TEST_CHECK(block_nr == i);
-		TEST_CHECK(tx_char == 0x06); // We received an ack
+		TEST_ASSERT(rx_packet(&xdm, data, sizeof(data), i, 0));
+		TEST_ASSERT(xmodem_server_process(&xdm, resp, &block_nr, ms_time()));
+		TEST_ASSERT(memcmp(data, resp, xdm.packet_size) == 0);
+		TEST_ASSERT(block_nr == i);
+		TEST_ASSERT(tx_char == 0x06); // We received an ack
 		tx_char = 0;
 	}
 	xmodem_server_rx_byte(&xdm, 0x04);
-	TEST_CHECK(xmodem_server_is_done(&xdm));
+	TEST_ASSERT(xmodem_server_is_done(&xdm));
 }
 
 static void test_errors(void) {
@@ -73,46 +76,46 @@ static void test_errors(void) {
 	uint8_t tx_char = 0;
 	int attempts = 0;
 
-	TEST_CHECK(xmodem_server_init(&xdm, tx_byte, &tx_char) >= 0);
+	TEST_ASSERT(xmodem_server_init(&xdm, tx_byte, &tx_char) >= 0);
 	for (uint32_t i = 0; i < 5 && !xmodem_server_is_done(&xdm); ) {
-		uint8_t data[XMODEM_PACKET_SIZE];
-		uint8_t resp[XMODEM_PACKET_SIZE];
+		uint8_t data[1024];
+		uint8_t resp[1024];
 		uint32_t block_nr;
 		memset(data, i, sizeof(data));
 		tx_char = 0;
 		// Inject 1:1000 rate of bad data bytes
-		rx_packet(&xdm, data, sizeof(data), i, 1000);
+		rx_packet(&xdm, data, sizeof(data), i, 5000);
 		if (xmodem_server_process(&xdm, resp, &block_nr, ms_time())) {
-			TEST_CHECK(memcmp(data, resp, sizeof(data)) == 0);
-			TEST_CHECK(block_nr == i);
+			TEST_ASSERT(memcmp(data, resp, sizeof(data)) == 0);
+			TEST_ASSERT(block_nr == i);
 			i++;
 		}
 		attempts++;
 	}
 	xmodem_server_rx_byte(&xdm, 0x04);
-	TEST_CHECK(xmodem_server_get_state(&xdm) == XMODEM_STATE_SUCCESSFUL);
-	TEST_CHECK(xmodem_server_is_done(&xdm));
+	TEST_ASSERT(xmodem_server_get_state(&xdm) == XMODEM_STATE_SUCCESSFUL);
+	TEST_ASSERT(xmodem_server_is_done(&xdm));
 }
 
 static void test_timeout(void) {
 	struct xmodem_server xdm;
 	uint8_t tx_char = 0;
-	TEST_CHECK(xmodem_server_init(&xdm, tx_byte, &tx_char) >= 0);
+	TEST_ASSERT(xmodem_server_init(&xdm, tx_byte, &tx_char) >= 0);
 
 	// Transmit 1 packet so we're not in the initial phase
-	uint8_t data[XMODEM_PACKET_SIZE] = {0};
+	uint8_t data[XMODEM_MAX_PACKET_SIZE] = {0};
 	rx_packet(&xdm, data, sizeof(data), 0, 100);
 
 	// We expect a packet every second, and currently limit ourselves to 10 errors
 	// so after 11 seconds we should be broken
 	for (int64_t start = 1; start < 11000; start++) {
-		uint8_t resp[XMODEM_PACKET_SIZE];
+		uint8_t resp[XMODEM_MAX_PACKET_SIZE];
 		uint32_t block_nr;
 		xmodem_server_process(&xdm, resp, &block_nr, start);
 	}
 	// The system should timeout
-	TEST_CHECK(xmodem_server_is_done(&xdm));
-	TEST_CHECK(xmodem_server_get_state(&xdm) == XMODEM_STATE_FAILURE);
+	TEST_ASSERT(xmodem_server_is_done(&xdm));
+	TEST_ASSERT(xmodem_server_get_state(&xdm) == XMODEM_STATE_FAILURE);
 }
 
 /**
@@ -175,25 +178,29 @@ static void tx_byte_fd(struct xmodem_server *xdm, uint8_t byte, void *cb_data)
 	(void)r;
 }
 
-static void test_sz(void) {
-	uint8_t output_data[4096] = {0};
-	uint8_t input_data[4096];
-	for (int i = 0; i < 4096; i++) {
+static void test_sz(bool use_1k, size_t data_size) {
+	uint8_t *input_data = malloc(data_size);
+	TEST_ASSERT(input_data != NULL);
+	uint8_t *output_data = malloc(data_size);
+	TEST_ASSERT(output_data != NULL);
+	for (size_t i = 0; i < data_size; i++) {
 		input_data[i] = rand();
 	}
 	char raw_data_name[20];
 	sprintf(raw_data_name, "/tmp/xmodem.XXXXXXX");
-	TEST_CHECK(mkstemp(raw_data_name) >= 0);
+	TEST_ASSERT(mkstemp(raw_data_name) >= 0);
 	FILE *fp = fopen(raw_data_name, "wb");
-	TEST_CHECK(fwrite(input_data, sizeof(input_data), 1, fp) == 1);
+	TEST_ASSERT(fwrite(input_data, data_size, 1, fp) == 1);
 	fclose(fp);
-	char * const args[] = {"sz", "--xmodem", "--quiet", raw_data_name, NULL};
+	char * const args_1k[] = {"lsz", "--xmodem", "--1k", "--quiet", raw_data_name, NULL};
+	char * const args_128[] = {"lsz", "--xmodem", "--quiet", raw_data_name, NULL};
+	char * const *args = use_1k ? args_1k : args_128;
 	int wr_fd = -1, rd_fd = -1;
 	struct xmodem_server xdm;
 	pid_t pid = spawn_process(args, &rd_fd, &wr_fd);
 	uint32_t block_nr;
-	TEST_CHECK(pid >= 0);
-	TEST_CHECK(xmodem_server_init(&xdm, tx_byte_fd, &wr_fd) >= 0);
+	TEST_ASSERT(pid >= 0);
+	TEST_ASSERT(xmodem_server_init(&xdm, tx_byte_fd, &wr_fd) >= 0);
 
 	while (!xmodem_server_is_done(&xdm)) {
 		fd_set rd_fds, wr_fds;
@@ -209,7 +216,7 @@ static void test_sz(void) {
 		tv.tv_usec = 1000;
 
 		if (select(max_fd + 1, &rd_fds, &wr_fds, NULL, &tv) >= 0) {
-			uint8_t resp[XMODEM_PACKET_SIZE];
+			uint8_t resp[XMODEM_MAX_PACKET_SIZE];
 			if (FD_ISSET(rd_fd, &rd_fds)) {
 				uint8_t buffer[32];
 				size_t count = read(rd_fd, buffer, sizeof(buffer));
@@ -218,28 +225,40 @@ static void test_sz(void) {
 				}
 			}
 			if (xmodem_server_process(&xdm, resp, &block_nr, ms_time())) {
-				memcpy(&output_data[block_nr * XMODEM_PACKET_SIZE], resp, XMODEM_PACKET_SIZE);
+				memcpy(&output_data[block_nr * xdm.packet_size], resp, xdm.packet_size);
 			}
 		}
 	}
-	TEST_CHECK(xmodem_server_get_state(&xdm) == XMODEM_STATE_SUCCESSFUL);
-	TEST_CHECK(block_nr + 1 == sizeof(input_data) / XMODEM_PACKET_SIZE);
+	TEST_ASSERT(xdm.packet_size == (use_1k ? 1024 : 128));
+	TEST_ASSERT(xmodem_server_get_state(&xdm) == XMODEM_STATE_SUCCESSFUL);
+	TEST_ASSERT(block_nr + 1 == data_size / xdm.packet_size);
 	waitpid(pid, NULL, 0);
 	unlink(raw_data_name);
-	for (unsigned i = 0; i < sizeof(input_data); i++) {
+	for (unsigned i = 0; i < data_size; i++) {
 		if (output_data[i] != input_data[i]) {
 			fprintf(stderr, "Diff at %u: 0x%x != 0x%x\n", i, output_data[i], input_data[i]);
 		}
 	}
-	TEST_CHECK(memcmp(output_data, input_data, sizeof(input_data)) == 0);
+	TEST_ASSERT(memcmp(output_data, input_data, data_size) == 0);
 	close(rd_fd);
 	close(wr_fd);
+	free(input_data);
+	free(output_data);
+}
+
+static void test_sz_128(void) {
+	test_sz(false, 2 * 1024 * 1024);
+}
+
+static void test_sz_1k(void) {
+	test_sz(true, 2 * 1024 * 1024);
 }
 
 TEST_LIST = {
 	{"simple", test_simple},
 	{"errors", test_errors},
 	{"timeout", test_timeout},
-	{"sz", test_sz},
+	{"lsz (128B)", test_sz_128},
+	{"lsz (1kB)", test_sz_1k},
 	{NULL, NULL},
 };
